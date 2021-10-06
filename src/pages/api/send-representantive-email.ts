2c9cb847505138b43ce544d10a38ae31ff30775e
Matheus <contato.mths@outlook.com>
2021-10-06 11:04:27 -0300

    feat: add client info to email

--- src/pages/api/send-representantive-email.ts
+++ src/pages/api/send-representantive-email.ts
@@ -17,15 +17,17 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     representantive,
     observation,
   }: RepresentativeData = req.body;
+
   const from = `Enviado por: ${representantive.name}, ${representantive.email}`;
   const date = `Enviado em: ${formatToBrazilianString(getDate())}`;
+  const cliente = `Cliente: ${client.name}, ${client.email}`;
   const list = [total, payment, conditions, term];
   if (observation) {
     list.push(observation);
   }
   const html = `
     <div style="font-family: Arial, Helvetica, sans-serif; color: #565857; font-weight: 600">
-      ${List([from, date])}  
+      ${List([from, date, cliente])}  
       ${Table([
         ['Referência', 'Quantidade', 'Subtotal'],
         ...getRecords(products),
